e10d44a54c26545ffd0691bafb3b9d081ddcb09e
GammaWolf <31368373+GammaWolf@users.noreply.github.com>
2021-08-13 14:09:48 +0200

    clicking on link doesn't stop audio player (which setting window.location.href does in FF)

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
   const [playing, setPlaying] = useState(null)
   const [lyricsInfo, setLyricsInfo] = useState(null)
   const [lyricsTextColumns, setLyricsTextColumns] = useState([])
+  const [mailLink, setMailLink] = useState('') // empty string so link appears clickable
 
   // one-time ui initialization
   useEffect(() => {
@@ -132,11 +133,12 @@ function App() {
             </svg>
           </button>
 
-          <button title="contact" onClick={contact} className="iconBtn">
+          {/* antispam: set mail href on click */}
+          <a title="contact" href={mailLink} onClick={setMailLinkToContactMail} className="iconBtn">
             <svg viewBox="0 0 24 24">
               <path d="M12.042 23.648c-7.813 0-12.042-4.876-12.042-11.171 0-6.727 4.762-12.125 13.276-12.125 6.214 0 10.724 4.038 10.724 9.601 0 8.712-10.33 11.012-9.812 6.042-.71 1.108-1.854 2.354-4.053 2.354-2.516 0-4.08-1.842-4.08-4.807 0-4.444 2.921-8.199 6.379-8.199 1.659 0 2.8.876 3.277 2.221l.464-1.632h2.338c-.244.832-2.321 8.527-2.321 8.527-.648 2.666 1.35 2.713 3.122 1.297 3.329-2.58 3.501-9.327-.998-12.141-4.821-2.891-15.795-1.102-15.795 8.693 0 5.611 3.95 9.381 9.829 9.381 3.436 0 5.542-.93 7.295-1.948l1.177 1.698c-1.711.966-4.461 2.209-8.78 2.209zm-2.344-14.305c-.715 1.34-1.177 3.076-1.177 4.424 0 3.61 3.522 3.633 5.252.239.712-1.394 1.171-3.171 1.171-4.529 0-2.917-3.495-3.434-5.246-.134z"></path>
             </svg>
-          </button>
+          </a>
         </div>
       </div>
 
@@ -216,13 +218,14 @@ function App() {
     return spotifyLink
   }
 
-  function contact() {
+  function setMailLinkToContactMail() {
+    // antispam
     let a = 'ma'
     let x = 'look.c'
     let m = 'zim_tigerclaw'.replace('_', '') + '\u0040out' + x + 'om'
     let o = 'o'
     let u = a + 'ilt' + o + ':' + m
-    window.location.href = u
+    setMailLink(u)
   }
 
   function focusActiveChannel() {
